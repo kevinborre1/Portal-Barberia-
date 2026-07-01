2b0d9c7dc542f880d809a83aaa782af34f4ab5fa
Kevin Borre <kevinborreboca@gmail.com>
2026-06-30 23:39:35 -0300

    agregando archivos de backend(springboot) y frontend

---
+++ barberBackend/BarHub/src/main/java/com/proyectoBarberHub/BarHub/BarHubApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoBarberHub.BarHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarHubApplication.class, args);
+	}
+
+}
---
+++ barberBackend/BarHub/src/test/java/com/proyectoBarberHub/BarHub/BarHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoBarberHub.BarHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
